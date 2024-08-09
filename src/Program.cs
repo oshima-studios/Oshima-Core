dca75aed8d48c98fa1541b44dd54458b41b1b740
milimoe <mili@wrss.org>
2024-08-09 19:28:58 +0800

    update

--- src/Program.cs
+++ src/Program.cs
@@ -1,55 +1,135 @@
 using Microsoft.AspNetCore.Diagnostics;
+using Milimoe.FunGame.Core.Api.Utility;
 using Milimoe.Oshima.Core.Configs;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-app.UseAuthorization();
+    WebApplication app = builder.Build();
 
-app.MapControllers();
-
-OSMCoreInit();
-
-app.Urls.Add("http://0.0.0.0:27799");
-
-app.UseExceptionHandler(errorApp =>
-{
-    errorApp.Run(async context =>
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        context.Response.StatusCode = 500;
-        context.Response.ContentType = "application/json";
-        IExceptionHandlerFeature? contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-        if (contextFeature != null)
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    OSMCoreInit();
+
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
         {
-            await context.Response.WriteAsync(new
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            IExceptionHandlerFeature? contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+            if (contextFeature != null)
             {
-                context.Response.StatusCode,
-                Message = "Internal Server Error.",
-                Detailed = contextFeature.Error.Message
-            }.ToString() ?? "");
+                await context.Response.WriteAsync(new
+                {
+                    context.Response.StatusCode,
+                    Message = "Internal Server Error.",
+                    Detailed = contextFeature.Error.Message
+                }.ToString() ?? "");
+            }
+        });
+    });
+
+    TaskUtility.NewTask(async () =>
+    {
+        while (true)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                if (now.Hour == 8 && now.Minute == 30 && !Daily.DailyNews)
+                {
+                    Daily.DailyNews = true;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                if (now.Hour == 8 && now.Minute == 31)
+                {
+                    Daily.DailyNews = false;
+                }
+                if (now.Hour == 0 && now.Minute == 0 && Daily.ClearDailys)
+                {
+                    Daily.ClearDailys = false;
+                    // 清空运势
+                    Daily.UserDailys.Clear();
+                    Daily.SaveDaily();
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("已重置所有人的今日运势。");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                if (now.Hour == 0 && now.Minute == 1)
+                {
+                    Daily.ClearDailys = true;
+                }
+                await Task.Delay(1000);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
         }
     });
-});
 
-app.Run();
+    TaskUtility.NewTask(() =>
+    {
+        while (true)
+        {
+            string read = Console.ReadLine() ?? "";
+            if (read == "quit" || read == "exit") break;
+            // OSM指令
+            if (read.Length >= 4 && read[..4].Equals(".osm", StringComparison.CurrentCultureIgnoreCase))
+            {
+                //MasterCommand.Execute(read, GeneralSettings.Master, false, GeneralSettings.Master, false);
+                Console.WriteLine("暂不支持OSM Core指令。");
+                continue;
+            }
+            switch (read.ToLower().Trim() ?? "")
+            {
+                case "debug on":
+                    GeneralSettings.IsDebug = true;
+                    Console.WriteLine("开启Debug模式");
+                    break;
+                case "debug off":
+                    GeneralSettings.IsDebug = false;
+                    Console.WriteLine("关闭Debug模式");
+                    break;
+            }
+        }
+    });
 
-static void OSMCoreInit(){
+    app.Run();
+}
+catch (Exception e)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(e);
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
+Console.ReadKey();
+
+static void OSMCoreInit()
+{
     GeneralSettings.LoadSetting();
     GeneralSettings.SaveConfig();
     Daily.InitDaily();
